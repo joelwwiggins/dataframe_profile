6bfcd29b73a1b000454df3354f0e39d37cd69b0a
Joel Wiggins <joelwwiggins@gmail.com>
2022-07-09 10:58:08 -0500

    local streamlit hello world

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.write('Hello World!')
+
+df = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)  # Same as st.write(df)
